d9f6eb82144b0a6b89c6ec69a324b3429de684cc
Diego Iturrieta Gajardo <diturrieta.g@gmail.com>
2023-05-01 03:09:55 -0400

    Indice de paquetes
    se indica a que libreria pertenece cada funcion

--- R/Estadistica descriptiva.R
+++ R/Estadistica descriptiva.R
@@ -7,28 +7,28 @@ datasets::swiss
 #datasests:: da paso a abrir todas las bases existentes en rstudio
 
 swiss #base de datos de suiza
-View(swiss) #abre una ventana para ver el set de datos
+tibble::view(swiss) #abre una ventana para ver el set de datos
 
 # Estadisticos descriptivos -----------------------------------------------
 #simples
-min(swiss$Fertility) #valor minimo de la variable fertility
-max(swiss$Fertility) #valor maximo de la varialbe fertility
-range(swiss$Fertility) #rango de la variable fertility
-mean(swiss$Fertility) #media/promedio de la variable fertility
-median(swiss$Fertility) #mediana/valor medio de la variable fertility
-length(swiss$Fertility) #numero observaciones de la variable
-sum(swiss$Fertility) #total de todas las observaciones/casillas del set de datos
-quantile(swiss$Fertility, 0.25) #cuantil 01, ojo si es con "," o "."
-quantile(swiss$Fertility, 0.75) #cuantil 03, ojo si es con "," o "."
-IQR(swiss$Fertility) #rango intercuartelario
+base::min(swiss$Fertility) #valor minimo de la variable fertility
+base::max(swiss$Fertility) #valor maximo de la varialbe fertility
+base::range(swiss$Fertility) #rango de la variable fertility
+base::mean(swiss$Fertility) #media/promedio de la variable fertility
+stats::median(swiss$Fertility) #mediana/valor medio de la variable fertility
+base::length(swiss$Fertility) #numero observaciones de la variable
+base::sum(swiss$Fertility) #total de todas las observaciones/casillas del set de datos
+stats::quantile(swiss$Fertility, 0.25) #cuantil 01, ojo si es con "," o "."
+stats::quantile(swiss$Fertility, 0.75) #cuantil 03, ojo si es con "," o "."
+stats::IQR(swiss$Fertility) #rango intercuartelario
 
 #algo elevados
-sd(swiss$Fertility) #desviación estandar de la variable fertility
-var(swiss) #varianza usando r base
-cor(swiss) #correlacion, usando r base
+stats::sd(swiss$Fertility) #desviación estandar de la variable fertility
+stats::var(swiss) #varianza usando r base
+stats::cor(swiss) #correlacion, usando r base
 
-
-
+#mas completos
+psych::describe()
 
 
 
