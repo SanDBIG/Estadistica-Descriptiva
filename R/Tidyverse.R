94015331f20c2acc2afd17635e32cc0be1ca081e
Diego Iturrieta Gajardo <diturrieta.g@gmail.com>
2023-05-01 03:20:10 -0400

    Añadi otro scrpt
    Separacion del ejercicio y la explicacion de la libreria tidyverse

---
+++ R/Tidyverse.R
@@ -0,0 +1,18 @@
+# Tidyverse ---------------------------------------------------------------
+# Tidyverse es un set de paquetes que trabajan de manera armoniosa ya que no tienen
+# conflicto entre ellos, los paquetes que se cargan a raiz de tidyverse son:
+
+# ggplot2 #para visualizar data
+# dplyr, #para manioular data
+# tidyr, #para ordenar data
+# readr, #para importar data
+# purrr, #para proramación funcional
+# tibble, #para ordenar los data frames o reconstruirlos
+# stringr, #para los strings/cadena de caracteres
+# forcats, #para factores
+# lubridate, #para fecha y horas
+
+pacman::p_load(tidyverse)
+
+# siempre al momento de inicializar un ambiente de r se deben
+# cargar las librerias que se van a usar dentro del mismo.
